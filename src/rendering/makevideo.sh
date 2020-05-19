07131d76ef87ccfc5b84d5d48afd90410d001e29
Kailash S <kailash.s.2012@gmail.com>
2020-05-18 22:23:10 -0500

    Bash script
    If compiling from source, this should be included in build directory as well

--- src/rendering/makevideo.sh
+++ src/rendering/makevideo.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# command: "./makevideo.sh " + frameRate + " '" + outputDirPath + "' " + width + "x" + height
+# command: ./makevideo.sh frameRate 'outputDirPath' widthxheight
 rate=$1
 path=$2
-pathim="'"+$path+"/output_%04d.png"+"'"
 size=$3
 echo $rate
 echo $path
 echo $size
-ffmpeg -r $rate -f image2 -s $size -i $pathim -vcodec libx264 -crf 25 -pix_fmt yuv420p "video_noaudio.mp4"
+cd "$path"
+cat *.png | ffmpeg -r $rate -f image2pipe -s $size -i - -vcodec libx264 -crf 25 -pix_fmt yuv420p "video_noaudio.mp4"
+# ffmpeg -r $rate -f image2 -s $size -i "$path" -vcodec libx264 -crf 25 -pix_fmt yuv420p "video_noaudio.mp4"
 ffmpeg -i video_noaudio.mp4 -i audio.mp3 -codec copy -shortest "video_withaudio.mp4"
